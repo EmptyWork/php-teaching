6af4a84b502f0e4671514c6cc8f7693d91991a4c
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-11-04 16:41:25 +0900

    Buat: 1-pengenalan-class

---
+++ 1-pengenalan-class.php
@@ -0,0 +1,37 @@
+<?php
+
+// abstarct ContohClass {
+class ContohClass {
+  public $dataPublic;
+  private $dataPrivate;
+  protected $dataProtected;
+
+  public function fungsiPublik() {
+    echo "Fungsi Publik\n";
+  }
+
+  public static function fungsiStatikPublik() {
+    echo "Fungsi yang di panggil langsung dari Kelas\n";
+  }
+
+  private function fungsiPrivate() {
+    echo "Fungsi Private\n";
+  }
+
+  protected function fungsiProtected() {
+    echo "Fungsi Protected\n";
+  }
+
+  final function fungsiFinal() {
+    echo "Fungsi Final\n";
+  }
+
+  // abstract public function fungsiAbstrak();
+}
+
+ContohClass::fungsiStatikPublik();
+$data = new ContohClass();
+$data->fungsiPublik();
+// $data->fungsiPrivate();
+// $data->fungsiProtected();
+$data->fungsiFinal();
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Object-Oriented pada PHP
+
+<a href="https://emptywork.netlify.app">
+  <img src="active.svg" width=100>
+</a>
+
+Source code untuk dasar-dasar tentang Object-Oriented pada PHP. Adapun materi yang termuat pada repository ini antara lain:
+- <a href="./1-pengenalan-class.php">Pengenalan Class [1-pengenalan-class.php]</a>
+  - Class, Abstract Class
+  - Properties dan Method
+    - Jenis-Jenis Properties dan Method
+    - Abstract Method
+  - Modifier
+    - Private
+    - Public
+    - Protected
+    - Abstract
+
+Didukung oleh:
+<a href="https://filkom.ukim.ac.id">Filkom UKIM</a> | <a href="https://ukim.ac.id">UKIM</a>
